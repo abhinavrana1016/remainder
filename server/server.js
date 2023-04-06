9ca070e51cd249a1ed602aa437fc57b1b4ff7a60
Varun Rana <abhinav.rana1016@gmail.com>
2023-04-06 23:07:28 +0530

    user model and routes

--- server/server.js
+++ server/server.js
@@ -2,12 +2,14 @@ const express = require('express')
 require('dotenv').config();
 const dbconfig = require("./config/db.config");
 const app = express()
+
 const port = process.env.PORT || 500
 app.listen(port,()=> console.log(`server is running on ${port}`))
-
+app.use(express.json());
 const route = require("./routes/routes")
 app.use(express.urlencoded({ extended: true }));
-app.use('/api',route)
+app.use('/api',route);
+
 app.get('*', (req, res) => {
     res.status(404).json({
         msg: 'Sorry, This route is not found on this server',
