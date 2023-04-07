d27a9711096b6b80d0bfeabe3877ac794c30d348
Varun Rana <abhinav.rana1016@gmail.com>
2023-04-07 06:48:40 +0530

    user registration model in backend

--- server/manager/user.manager.js
+++ server/manager/user.manager.js
@@ -1,11 +1,42 @@
-const userModel = require("../models/user.model")
-const registerUser = async(body)=> {
-    try {
-        console.log(body)
-    } catch (error) {
-        console.log(error)
+const userModel = require("../models/user.model");
+var bcrypt = require("bcryptjs");
+const { Success, Error, success } = require('../util/response.util');
+const registerUser = async (body) => {
+  try {
+    console.log(body);
+    var salt = bcrypt.genSaltSync(10);
+    var hashPassword = bcrypt.hashSync(body?.password, salt);
+    // check for duplicate email,mobile no, userName
+    const emailExist = await userModel.exists({ email: body?.email });
+    const mobileExist = await userModel.exists({ mobileno: body?.mobileno });
+    const userName = await userModel.exists({ userName: body?.userName });
+    if (emailExist) {
+        console.log("emil")
+      return Error({ message: "email already exist" });
     }
-}
+    else if (mobileExist) {
+
+      return Error({ message: "mobile no already exist" });
+    }
+    else if (userName) {
+      return Error({ message: "user Name already exist" });
+    }
+    else {
+        await userModel({
+            firstName:body?.firstName,
+            lastName:body?.lastName,
+            userName:body?.userName,
+            email:body?.email,
+            mobileno:body?.mobileno,
+            password:hashPassword,
+            dateofbirth:body?.dateofbirth,
+        }).save()
+        return success({ message: "Registration successfully" });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
 module.exports = {
-    registerUser
-}
+  registerUser,
+};
--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose')
 const { Schema } = mongoose;
 const userSchema = new mongoose.Schema({
-    username:{type:String,require:true},
+    firstName:{type:String,require:true},
+    lastName:{type:String,require:true},
+    userName:{type:String,require:true},
     email:{type:String,require:true},
     password:{type:String,require:true},
     dateofbirth:{type:Date,require:true},
